d960506ca47a9da3f25ca6ac48d80ef77b242828
Nikolaos Benakis <nickolasbenele@gmail.com>
2024-07-20 23:52:21 +0300

    Add fallback

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
   title: "Funnels Preview",
   description: "Funnels Preview",
-  url: process.env.NEXT_PUBLIC_SITE_URL,
+  url: process.env.NEXT_PUBLIC_SITE_URL || "",
 };
