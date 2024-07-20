2c5fa866d957542028cf4c1e7fe140f4c2a77513
Nikolaos Benakis <nickolasbenele@gmail.com>
2024-07-20 13:28:03 +0300

    Test lint

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 
 export const Footer = () => {
+  asdaspdasdas;
   return (
     <footer className="text-muted-foreground absolute bottom-2 w-full text-center text-sm">
       © {new Date().getFullYear()} By{" "}
