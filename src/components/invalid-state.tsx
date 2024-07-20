380b3276f6000b836c86a1436958e1bdf63f03cc
Nikolaos Benakis <nickolasbenele@gmail.com>
2024-07-21 00:47:34 +0300

    Add styles to json view

--- src/components/invalid-state.tsx
+++ src/components/invalid-state.tsx
@@ -1,24 +1,25 @@
 import Info from "@/components/info";
 import exampleJSON from "@/json/example.json";
-import React, { Suspense } from "react";
+import React from "react";
 import * as reactJsonView from "react-json-view-lite";
-import { Skeleton } from "./ui/skeleton";
 
 import "react-json-view-lite/dist/index.css";
 
 const InvalidState = () => {
   return (
-    <Suspense fallback={<Skeleton />}>
+    <>
       <Info
         heading="Invalid json schema"
         subheading="Try to upload another json, similar to the json below 👇"
       />
-      <reactJsonView.JsonView
-        data={exampleJSON}
-        shouldExpandNode={reactJsonView.allExpanded}
-        style={reactJsonView.darkStyles}
-      />
-    </Suspense>
+      <div className="mx-auto w-[80%]">
+        <reactJsonView.JsonView
+          data={exampleJSON}
+          shouldExpandNode={reactJsonView.allExpanded}
+          style={reactJsonView.darkStyles}
+        />
+      </div>
+    </>
   );
 };
 
