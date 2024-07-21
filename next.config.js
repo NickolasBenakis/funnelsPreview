5e5d8f6d49138f45d74b80f4eb080608175aff55
Nikolaos Benakis <nickolasbenele@gmail.com>
2024-07-21 17:45:26 +0300

    Add pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  mode: 'production',
+});
+
 const nextConfig = {
 	reactStrictMode: false,
   productionBrowserSourceMaps: true,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,60 @@ const RootLayout = ({ children }: PropsWithChildren) => {
     <html lang={"en"} suppressHydrationWarning={true}>
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon/favicon-16x16.png" />
+      <link
+        rel="apple-touch-startup-image"
+        href="/favicon/apple-touch-icon.png"
+        sizes="512x512"
+      />
+      <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+      <meta name="application-name" content="Funnels Preview" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="PWA App" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-TileColor" content="#FBF7EF" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="theme-color" content="#FBF7EF" />
+      <link rel="manifest" href="/manifest.json" />
+
+      <link
+        rel="apple-touch-startup-image"
+        href="/favicon/apple-touch-icon.png"
+        sizes="2048x2732"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/favicon/apple-touch-icon.png"
+        sizes="1668x2224"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/favicon/apple-touch-icon.png"
+        sizes="1536x2048"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/favicon/apple-touch-icon.png"
+        sizes="1125x2436"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/favicon/apple-touch-icon.png"
+        sizes="1242x2208"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/favicon/apple-touch-icon.png"
+        sizes="750x1334"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/favicon/apple-touch-icon.png"
+        sizes="640x1136"
+      />
+
       <body className={cn("min-h-screen font-sans", fonts)}>
         <ThemeProvider attribute="class">
           <Navbar />
